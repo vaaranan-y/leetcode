e8a5686df1b0bed01ddc449ed19dd584610ef2fe
Vaaranan Yogalingam <vaaranan.y@gmail.com>
2024-11-21 20:59:54 -0500

    Fixed leetcode 55 solution

--- 55/55.py
+++ 55/55.py
@@ -1,8 +1,8 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        toReach = nums[len(nums) - 1] # This is where we need to get to currently (starts as the last index, which is our goal)
+        toReach = len(nums) - 1 # This is where we need to get to currently (starts as the last index, which is our goal)
 
-        for i in range(len(nums) - 1, -1, -1):
+        for i in range(len(nums) - 2, -1, -1):
             if(i + nums[i] >= toReach): # Check if we can reach our goal from current index (i.e. the furthest index we can reach is the current index plus the jump distance)
                 toReach = i # Now we have a new goal, which is the current index, since the current index can reach our original goal
         
