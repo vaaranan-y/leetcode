28853c0720620deac2332597ae0238584a368d32
Vaaranan Yogalingam <vaaranan.y@gmail.com>
2024-12-20 00:00:26 -0500

    Solved leetcode 162

---
+++ 162/162.py
@@ -0,0 +1,19 @@
+class Solution:
+    def findPeakElement(self, nums: List[int]) -> int:
+        if(len(nums) == 1):
+            return 0
+        l = 0
+        r = len(nums) - 1
+
+        while(l <= r):
+            m = (l + r)//2
+
+            if(m > 0 and nums[m] < nums[m - 1]):
+                r = m - 1
+            elif(m < len(nums) - 1 and nums[m] < nums[m + 1]):
+                l = m + 1
+            else:
+                return m
+
+        
+
